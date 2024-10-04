5ecab5767c7859a965a943527fe0020dd5d6b5da
Aaron Snowberger <jekkilekki@gmail.com>
2024-10-04 11:00:27 +0900

    Week 5

---
+++ Week 5/for.c
@@ -0,0 +1,19 @@
+// for.c
+#include <stdio.h>
+
+int main(void) {
+	int sum = 0, num;
+
+	for (num = 1; num <= 5; num++) {
+		printf("num(%d) + sum(%d) = ", num, sum);
+
+		sum = sum + num; // »õ·Î ÃÑ°è°ª
+
+		printf("%d\n", sum);
+	}
+
+	printf("Final: num = %d, sum = %d", num, sum);
+
+	getch();
+	return 0;
+}
